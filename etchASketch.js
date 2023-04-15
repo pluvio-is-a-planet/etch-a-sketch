9c6fc2baa7251a0d9de589bf0124c95be5ef9328
Rynhardt Engelbrecht <107496937+pluvio-is-a-planet@users.noreply.github.com>
2023-04-15 12:54:28 +0200

    Create function to draw 16x16 grid
    Made the function drawGrid with no parameters, that iterates through a
    two-dimensional for loop, that creates a 16 by 16 grid of div elements
    and then places it in the DOM.

--- etchASketch.js
+++ etchASketch.js
@@ -2,3 +2,30 @@
 const gridBox = document.createElement('div');
 
 gridBox.className = 'grid box';
+
+// Create a 16x16 grid by adding gridBox elements to the .grid.container class
+// Assign .grid.container class to a const
+const gridContainer = document.querySelector('.grid.container');
+
+// Nested for loops to add gridBox elements to gridContainer
+// for i < 16 { for i < 16 { add box }}
+
+function drawGrid() {
+  for (let row = 0; row < 16; row++) {
+
+  const gridRow = document.createElement('div');
+  gridRow.className = 'grid row';
+
+  for (let col = 0; col < 16; col++) {
+
+    gridRow.id = `row-${row + 1}`;
+    gridRow.appendChild(gridBox.cloneNode(true));
+
+  }
+
+  gridContainer.appendChild(gridRow.cloneNode(true));
+
+  }
+}
+
+drawGrid();
